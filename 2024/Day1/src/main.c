61ced3958fbc46b608fb3bc91057d3d0cda19e51
Jonathan Balat <Jonathan.Balat@analog.com>
2025-02-27 17:03:01 +0800

    Removed main header file;

--- 2024/Day1/src/main.c
+++ 2024/Day1/src/main.c
@@ -1,12 +1,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "common.h"
 
 int main(void)
 {
-    /* Code goes here */
-    printf("Hello, World!\n");
+    char line[256];
+
+    FILE *file = NULL;
+    
+    printf("AoC Day1 Puzzle 1!\n");
+
+    file = get_input_data("../input.txt");
+    
+    while (fgets(line, sizeof(line), file)) 
+    {
+        // Process each line
+        printf("%s\n", line);
+    }
+
+    fclose(file);
+    // loop over each line input
+    //      - Get integers into two lists.
+    // Sort list by value.
+    // Contrast each index to get their difference. Store to 3rd list.
+    // find total distance and return result.
 
     return 0;
 }
