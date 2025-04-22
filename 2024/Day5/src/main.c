1af16b2ce83ceeb4dfc00c34ac033a24cc4a7bce
Jonathan Balat <jonathan.balat@analog.com>
2025-04-22 11:13:37 +0800

    Commit sample input; Commit base code for puzzle 1

--- 2024/Day5/src/main.c
+++ 2024/Day5/src/main.c
@@ -3,14 +3,18 @@
 #include <stdint.h>
 #include "common.h"
 
+#define MAX_CHAR (70)
 
-/* PUZZLE 1 FUNCTIONS */
-
-/* PUZZLE 2 FUNCTIONS */
+//Create linklist sctructure
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
 
 
 int main(void)
 {
+    /****** Read input.txt ******/
     FILE *file = fopen("../input.txt", "r");
 
     if (file == NULL) {
@@ -18,11 +22,33 @@ int main(void)
         return EXIT_FAILURE;
     }
 
-    /* Puzzle 1 Scan */
+    /****** Initialize linked list ******/
+    Node base = {.data=0, .next=NULL};
+    Node *head = &base;
 
+    // If number does not exist, create node and append.
+    // If number exists, insert node after number
+
+    /* Extract Page rules */
+    char *get_line = (char *)malloc((MAX_CHAR) * sizeof(char));
+    while (fgets(get_line, MAX_CHAR, file) != NULL)
+    {
+        /* Check for empty line - end of Page rules */
+        if (strlen(get_line) <= 1)
+        {
+            break;
+        }
+        printf("%s", get_line);
+    }
+
+    /* Extract Page updates */
+    printf("\n\nPAGE UPDATES\n\n");
+    while (fgets(get_line, MAX_CHAR, file) != NULL)
+    {
+        printf("%s", get_line);
+    }
     fclose(file);
-
-    printf("Day 5\n");
 
     return 0;
 }
+
