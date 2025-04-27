caad65918c57cc7f184b7fcc5ebaa21a05366774
Jonathan Balat <engrjonathanbalat@gmail.com>
2025-04-28 00:27:09 +0800

    Commit working solution to puzzle 1

--- 2024/Day6/python/solution.py
+++ 2024/Day6/python/solution.py
@@ -8,13 +8,65 @@ def generate_map(ref_file) -> list:
 
     return map_arr
 
-def project_line(map_arr:list) -> list:
-    ...
-    return []
 
-def turn_direction(map_arr: list) -> list:
-    ...
-    return []
+def project_line(map_arr:list, curr_pos: tuple) -> tuple:
+    tr_x, tr_y, tr_dir = curr_pos
+
+    dx, dy = 0, 0  # next move
+    x, y = 0, 0    # committed move
+    b_exited = False
+    b_collision = False
+    while not b_collision and not b_exited:
+        x, y = dx, dy
+
+        # Get copy of current row before changes
+        row = map_arr[tr_y + dy]
+
+        # Sets sign based on tr_dir
+        if tr_dir == DIR_UP:
+            dy -= 1
+        elif tr_dir == DIR_RIGHT:
+            dx += 1
+        elif tr_dir == DIR_DOWN:
+            dy += 1
+        else:  # DIR_LEFT
+            dx -= 1
+
+        # Verifies if found Wall, set new character
+        try:
+            if map_arr[tr_y + dy][tr_x + dx] == CHAR_WALL:
+                b_collision = True
+
+                # Update character
+                dir_char, tr_dir = turn_direction(tr_dir)
+                map_arr[tr_y + y] = row[:(tr_x + x)] + dir_char + row[(tr_x + x + 1):]
+            else:  # Else set walked character
+                map_arr[tr_y + y] = row[:(tr_x + x)] + CHAR_WALKED + row[(tr_x + x + 1):]
+        except IndexError:
+            b_exited = True
+            map_arr[tr_y + y] = row[:(tr_x + x)] + CHAR_WALKED + row[(tr_x + x + 1):]
+
+    # Commit steps to current position
+    curr_pos = (tr_x + x, tr_y + y, tr_dir)
+
+    return curr_pos, b_exited
+
+def turn_direction(direction: int) -> tuple:
+
+    direction += 1
+    if direction > DIR_LEFT:
+        direction = 0
+
+    if direction == DIR_UP:
+        character = '^'
+    elif direction == DIR_RIGHT:
+        character = '>'
+    elif direction == DIR_DOWN:
+        character = 'v'
+    else:  # DIR_LEFT
+        character = '<'
+
+    return character, direction
 
 if __name__ == "__main__":
 
@@ -30,52 +82,18 @@ if __name__ == "__main__":
 
         map_array = generate_map(file)
 
-        for item in map_array:
-            print(item)
-
-        # First time, get position
+        # First time, get position when found
         for y_pos,item in enumerate(map_array):
             x_pos =  item.find("^")
             if x_pos > -1:
                 curr_position = (x_pos, y_pos, DIR_UP)
 
-        print(curr_position)
-        tr_x, tr_y, dir = curr_position
+        b_done = False
+        while not b_done:
+            curr_position, b_done = project_line(map_array, curr_position)
 
-        # Determine direction
-        dx, dy = 0, 0
-
-        b_collision = False
-        while not b_collision:
-            x, y = dx, dy
-
-            map_array[tr_y + dy] = map_array[tr_y + dy][:(tr_x + dx)] + CHAR_WALKED + map_array[tr_y + dy][(tr_x + dx+1):]
-
-            # Sets sign based on direction
-            if dir == DIR_UP:
-                dy -= 1
-            elif dir == DIR_RIGHT:
-                dx += 1
-            elif dir == DIR_DOWN:
-                dx -= 1
-            else:  # DIR_LEFT
-                dy += 1
-
-            # print(dx, dy)
-
-            # Verifies if found Wall
-            if map_array[tr_y+dy][tr_x+dx] == CHAR_WALL:
-                map_array[tr_y + dy] = map_array[tr_y + dy][:(tr_x + dx)] + CHAR_WALL + map_array[tr_y + dy][
-                                                                                          (tr_x + dx + 1):]
-                b_collision = True
-
-
-
-        # Commit steps to current position
-        tr_y += y
-        tr_x += x
-
-        print(tr_x, tr_y)
-
+        sum_walked = 0
         for item in map_array:
-            print(item)
+            sum_walked += item.count(CHAR_WALKED)
+            # print(item)
+        print("Total walked spots =", sum_walked)
